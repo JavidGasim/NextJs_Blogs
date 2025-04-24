e6a1f667e92fb6c2f62d2e21011f869036eeaadb
Qasimov Cavid Mushviq <Qasim_ej47@itstep.edu.az>
2025-04-24 11:43:32 +0400

    all ready

---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+import {useThemeStore} from "../store";
+
+const ThemeToggle = () => {
+  const theme = useThemeStore((state) => state.theme);
+  const themeToggle = useThemeStore((state) => state.themeToggle);
+
+  return (
+    <button
+      onClick={themeToggle}
+      className={`w-[60px] h-[30px] rounded-full ${theme ? "bg-[#4B6BFB]" : "bg-gray-300"} relative transition-colors duration-300`}
+    >
+      <div
+        className={`w-[24px] h-[24px] rounded-full bg-white absolute top-[3px] transition-all duration-300 ${
+          theme ? "left-[32px]" : "left-[3px]"
+        }`}
+      />
+    </button>
+  );
+};
+
+export default ThemeToggle;
